edc67e6626c467f75bd2444fe402e4d189c2fa0d
jamesotto852 <jamesotto852@gmail.com>
2022-01-28 20:50:59 +0000

    Add multiplication and division tables as exported data sets

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Multiplication table
+#'
+#' A long tidy tibble containing a multiplication table
+#'
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'   \item{x}{Multiplicand}
+#'   \item{y}{Multiplier}
+#'   \item{z}{Product}
+#' }
+"multiplication_table"
+
+#' Division table
+#'
+#' A long tidy tibble containing a division table
+#'
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'   \item{x}{Numerator}
+#'   \item{y}{Denominator}
+#'   \item{z}{Quotient}
+#' }
+"division_table"
